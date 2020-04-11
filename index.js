d08677af0caaae1379b73fea1d16ab2368d50fad
Joey Hendricks <joeysguitartabs@yahoo.com>
2020-04-11 01:04:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ function add() {
 }
 
 function subtract() {
-
+  return 60 - 40;
 }
 
 function multiply() {
-
+  return 2 * 3.4;
 }
 
 function divide() {
-
+  return 5.0 / 2.5;
 }
 
 var number = 10;
